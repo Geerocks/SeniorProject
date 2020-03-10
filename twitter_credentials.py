11e3692253dec2af54bc178568add0d981c6143e
Geerocks <54655165+Geerocks@users.noreply.github.com>
2020-03-09 17:21:40 -0700

    Update twitter_credentials.py

--- twitter_credentials.py
+++ twitter_credentials.py
@@ -1,5 +1,7 @@
 #variables that contain the user credentials to access Twitter API
+
 ACCESS_TOKEN = "68491006-g5qhhCcvQqGM7hmSPKoRBjMyL2nG3ryk1dsUVEUKo"
 ACCESS_TOKEN_SECRET = "hueMWPssCHEgV0HwdPaJYuZGdtKV4g79V4FnEFFWuvSLf"
 CONSUMER_KEY = "t6jSdRu6YKVYDzRmOzodOimKP"
 CONSUMER_SECRET = "ONAi7xpDfNy5Rcf369Ysdcskt7mjpj06CddiJuRjYQzh29ak5B"
+
