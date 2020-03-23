2c2758720fed317c4689421e74811634cfe082b3
Geerocks <54655165+Geerocks@users.noreply.github.com>
2020-03-23 15:28:02 -0700

    Changed R file

--- test.R
+++ test.R
@@ -1,3 +1,22 @@
-install.packages(dbConnect, type = "source")
-library(RMySQL)
+install.packages("RMySQL")
+install.packages("RMariaDB")
+install.packages("DBI")
+install.packages("odbc")
+install.packages("RSQLite")
+install.packages("devtools")
+devtools::install_github("r-dbi/DBI")
+library(odbc)
 library(dbConnect)
+library(DBI)
+con <- dbConnect(RMySQL::MySQL(),
+    dbname = "TwitterDB",
+    host = "localhost",
+    port = 3306,
+    user = "root",
+    password = "gaurav")
+
+res <- dbSendQuery(con, "SELECT created_at, polarity FROM tesla")
+str(res)
+
+dbFetch(res, 5)
+dbDisconnect(con)
