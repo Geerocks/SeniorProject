bfb0d7a05ed012c97c0078bb912b139a1f29deae
Geerocks <54655165+Geerocks@users.noreply.github.com>
2020-04-17 00:17:54 -0700

    eth

--- debug.py
+++ debug.py
@@ -12,22 +12,7 @@ mydb = mysql.connector.connect(
     charset = 'utf8'
 )
 count = 0
-while True:
 
-    if mydb.is_connected():
-        mycursor = mydb.cursor()
-        """mycursor.execute("CREATE TABLE {} ({})".format(settings.TABLE_NAME, "price VARCHAR(255), date DATETIME"))"""
-        if (count < 1):
-            mycursor.execute("CREATE TABLE {} ({})".format(settings.TABLE_NAME, "price VARCHAR(255), date DATETIME"))   
-            count += 1
-        sql = "INSERT INTO {} (price, date) VALUES (%s, %s)".format(settings.TABLE_NAME)
-        p= si.get_top_crypto()
-        price = p["Price (Intraday)"][0]
-        i = datetime.datetime.now()
-        date = ("%s" % i)
-    
-        val = (price, date)
-        mycursor.execute(sql,val)
-        mydb.commit()
-        mycursor.close()
-    time.sleep(60)
+p= si.get_top_crypto()
+price = p["Price (Intraday)"][0]
+print(price)
