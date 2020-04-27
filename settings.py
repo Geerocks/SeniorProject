5a8aeb9d8ec611c94739b5b4c1933779d2f8b1d0
Geerocks <54655165+Geerocks@users.noreply.github.com>
2020-04-27 14:17:39 -0700

    4

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
-TRACK_WORDS = ['snapchat']
-TABLE_NAME = "snapchat"
+TRACK_WORDS = ['stuff']
+TABLE_NAME = "stuff"
 TABLE_ATTRIBUTES = "id_str VARCHAR(255), created_at DATETIME, text VARCHAR(255), \
             polarity INT, subjectivity INT, user_created_at VARCHAR(255), user_location VARCHAR(255), \
             user_description VARCHAR(255), user_followers_count INT, longitude DOUBLE, latitude DOUBLE, \
