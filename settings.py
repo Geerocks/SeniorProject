121097cdf067facb5f42f171dc3fff950eb1923f
Geerocks <54655165+Geerocks@users.noreply.github.com>
2020-05-04 15:02:10 -0700

    7

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
-TRACK_WORDS = ['starbucks']
-TABLE_NAME = "starbucks"
+TRACK_WORDS = ['tesla']
+TABLE_NAME = "tesla"
 TABLE_ATTRIBUTES = "id_str VARCHAR(255), created_at DATETIME, text VARCHAR(255), \
             polarity INT, subjectivity INT, user_created_at VARCHAR(255), user_location VARCHAR(255), \
             user_description VARCHAR(255), user_followers_count INT, longitude DOUBLE, latitude DOUBLE, \
